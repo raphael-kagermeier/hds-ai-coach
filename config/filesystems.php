06d07d3da3358488c2b534b8bec6d1d4fe016e4b
Raphael Kagermeier <44846337+raphael-kagermeier@users.noreply.github.com>
2024-12-11 15:20:50 +0100

    initial serverless framework setup

--- config/filesystems.php
+++ config/filesystems.php
@@ -47,13 +47,13 @@ return [
 
         's3' => [
             'driver' => 's3',
-            'key' => env('AWS_ACCESS_KEY_ID'),
-            'secret' => env('AWS_SECRET_ACCESS_KEY'),
-            'region' => env('AWS_DEFAULT_REGION'),
-            'bucket' => env('AWS_BUCKET'),
-            'url' => env('AWS_URL'),
-            'endpoint' => env('AWS_ENDPOINT'),
-            'use_path_style_endpoint' => env('AWS_USE_PATH_STYLE_ENDPOINT', false),
+            'key' => env('PROJECT_AWS_ACCESS_KEY_ID'),
+            'secret' => env('PROJECT_AWS_SECRET_ACCESS_KEY'),
+            'region' => env('PROJECT_AWS_DEFAULT_REGION'),
+            'bucket' => env('PROJECT_AWS_BUCKET'),
+            'url' => env('PROJECT_AWS_BUCKET_URL'),
+            'endpoint' => env('PROJECT_AWS_ENDPOINT'),
+            'use_path_style_endpoint' => env('PROJECT_AWS_USE_PATH_STYLE_ENDPOINT', false),
             'throw' => false,
         ],
 

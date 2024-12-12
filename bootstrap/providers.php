eb96a5746ab07d62c2e4c6b9c1015e3ace729002
Raphael Kagermeier <44846337+raphael-kagermeier@users.noreply.github.com>
2024-12-12 18:02:38 +0100

    improved config handling

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    App\Providers\ProjectConfigServiceProvider::class,
     App\Providers\AppServiceProvider::class,
     App\Providers\Filament\AdminPanelProvider::class,
     App\Providers\Filament\AppPanelProvider::class,

05485d3e2c9cb4e9375b3f879cfeafae084c31eb
Raphael Kagermeier <44846337+raphael-kagermeier@users.noreply.github.com>
2024-12-15 17:04:14 +0100

    New home route

---
+++ resources/views/filament/pages/home.blade.php
@@ -0,0 +1,7 @@
+<x-filament-panels::page>
+    <div class="flex mx-auto flex-col text-center max-w-lg">
+        <span class="text-3xl text-balance ">{!! $this->quote[0] !!}</span>
+        <span> ~ {!! $this->quote[1] !!}</span>
+    </div>
+
+</x-filament-panels::page>

dc91eb2c572420b36c608124caac0afb92548a7e
Raphael Kagermeier <44846337+raphael-kagermeier@users.noreply.github.com>
2025-01-09 13:31:45 +0100

    home redirect

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,6 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('health-check', \Spatie\Health\Http\Controllers\SimpleHealthCheckController::class);
 Route::get('robots.txt', App\Http\Controllers\RobotsController::class);
+
+Route::redirect('/app', '/app/generations/generate');
+Route::redirect('/', '/app');

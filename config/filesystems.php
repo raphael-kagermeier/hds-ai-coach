5b00f54532cddb41b53088766092c5d262d169c5
raphael-kagermeier <raphael-kagermeier@users.noreply.github.com>
2025-01-10 11:49:02 +0000

    Fixes coding style

--- config/filesystems.php
+++ config/filesystems.php
@@ -64,7 +64,7 @@ return [
             ] : [
                 'driver' => 'local',
                 'root' => storage_path('app/public'),
-                'url' => env('APP_URL') . '/storage',
+                'url' => env('APP_URL').'/storage',
             ]
         ),
 

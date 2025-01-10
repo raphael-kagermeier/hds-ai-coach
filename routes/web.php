6683ff9e8793dd9c70a46f198864d3fa23664ab6
Raphael Kagermeier <44846337+raphael-kagermeier@users.noreply.github.com>
2025-01-10 20:21:50 +0100

    removed web debug response

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,3 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('health-check', \Spatie\Health\Http\Controllers\SimpleHealthCheckController::class);
 Route::get('robots.txt', App\Http\Controllers\RobotsController::class);
-
-Route::get('/test', function () {
-    dd(config('filesystems'));
-});

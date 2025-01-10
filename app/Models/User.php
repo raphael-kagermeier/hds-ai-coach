5f04829f3bee2b09531d9638cd87ccf59ccfffb4
raphael-kagermeier <raphael-kagermeier@users.noreply.github.com>
2025-01-10 13:19:51 +0000

    Fixes coding style

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,12 +4,12 @@ namespace App\Models;
 
 use App\Traits\HasSuperAdmin;
 use Filament\Models\Contracts\FilamentUser;
+use Filament\Models\Contracts\HasAvatar;
 use Filament\Panel;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Illuminate\Support\Facades\Storage;
-use Filament\Models\Contracts\HasAvatar;
 
 class User extends Authenticatable implements FilamentUser, HasAvatar
 {

ccef2d6e3dbe7a9e3bc37d7e38060cd4176760a4
Raphael Kagermeier <44846337+raphael-kagermeier@users.noreply.github.com>
2025-01-10 16:10:39 +0100

    testing private

--- app/Models/User.php
+++ app/Models/User.php
@@ -43,6 +43,6 @@ class User extends Authenticatable implements FilamentUser, HasAvatar
 
     public function getFilamentAvatarUrl(): ?string
     {
-        return $this->avatar_url ? Storage::disk('public')->url($this->avatar_url) : null;
+        return $this->avatar_url ? Storage::disk('private')->url($this->avatar_url) : null;
     }
 }

9ff0b2c468446d4917ec079e2b406141c04af154
Raphael Kagermeier <44846337+raphael-kagermeier@users.noreply.github.com>
2025-01-10 16:52:04 +0100

    /test

--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('health-check', \Spatie\Health\Http\Controllers\SimpleHealthCheckController::class);
 Route::get('robots.txt', App\Http\Controllers\RobotsController::class);
+
+Route::get('/test', function () {
+    dd(config('filesystems'));
+});

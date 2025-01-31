f382fad571c7a8f2b8c659fcf68c896d4819e698
Raphael Kagermeier <44846337+raphael-kagermeier@users.noreply.github.com>
2025-01-31 09:10:56 +0100

    Refine health checks configuration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -41,10 +41,9 @@ class AppServiceProvider extends ServiceProvider
 
         Health::checks([
             DatabaseCheck::new(),
-            CacheCheck::new()
-                ->checkViews()
+            CacheCheck::new(),
+            OptimizedAppCheck::new()
                 ->checkConfig(),
-            OptimizedAppCheck::new(),
             // ScheduleCheck::new(),
             SecurityAdvisoriesCheck::new(),
         ]);
